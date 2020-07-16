0e0f2f6cc14d9c01d8d4566f7de91da7cd926331
taironecdias <taironecdias@gmail.com>
2020-07-16 19:28:43 -0300

    adicionando tradução para pt-BR

---
+++ passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'As senhas devem conter pelo menos 8 caracteres.',
+    'reset' => 'Sua senha foi alterada!',
+    'sent' => 'Enviamos para seu e-mail um link de redefinição de senha!',
+    'throttled' => 'Aguarde um momento e tente novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não encontramos um usuário com esse endereço de e-mail.",
+
+];
